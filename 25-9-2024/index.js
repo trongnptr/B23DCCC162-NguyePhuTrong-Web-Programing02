4b633302a9846a08487b8b9339684b019b55f0f7
trongnptr <trongnp.itds@gmail.com>
2024-10-21 14:30:24 +0700

    first

---
+++ 25-9-2024/index.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+const Counter2 = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+};
+
+export default Counter2;
---
+++ 25-9-2024/index2.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  handleClick = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>You clicked {this.state.count} times</p>
+        <button onClick={this.handleClick}>Click me</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
