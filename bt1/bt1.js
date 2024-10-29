de66dd09276924e122995893120f16694cc7777c
trongdz <trongnp.itds@gmail.com>
2024-10-29 21:18:04 +0700

    trongdz

---
+++ bt1/bt1.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3003;
+  
+app.get('/students', (req, res) => {
+  res.json([
+    { id: 1, name: 'John Doe', age: 20 },
+    { id: 2, name: 'Jane Smith', age: 22 },
+    { id: 3, name: 'Mark Johnson', age: 21 }
+  ]);
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
